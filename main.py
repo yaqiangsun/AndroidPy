ae8bbfc7f70088e57d5c456a8cc879d677b04eaf
xiaolang <sunyaking@163.com>
2020-11-30 14:41:25 +0800

    add chinese support

--- main.py
+++ main.py
@@ -1,8 +1,16 @@
+import kivy
 from kivy.app import App
 from kivy.uix.button import Button
+from kivy.uix.label import Label
+from kivy.core.window import Window
+
+
+kivy.resources.resource_add_path("./data")
+font1=kivy.resources.resource_find("SourceHanSansSC-Light.otf")
+
 
 class TestApp(App):
     def build(self):
-        return Button(text='Hello World')
+        return Button(text='Hello World!你好',font_name=font1)
 
 TestApp().run()
